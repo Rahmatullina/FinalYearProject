976487af927e8ea0146510ec406110e5058722c8
Emiliya <strongamil1998@gmail.com>
2020-03-21 15:02:24 +0300

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -2,10 +2,3 @@ from django.test import TestCase
 import unittest
 # Create your tests here.
 
-from django.test import Client
-
-class SimpleTest(unittest.TestCase):
-    def test_details(self):
-        client = Client()
-        response = client.get('/make_recognition/')
-        self.assertEqual(response.status_code, 200)
